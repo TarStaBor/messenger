25c89364ee4594f76385f3d564367670aa76ec26
TarStaBor <TarStaBor@yandex.ru>
2024-01-17 19:02:56 +0300

    feat: add config files & dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import {defineConfig} from "vite";
 import handlebars from "vite-plugin-handlebars";
+import checker from "vite-plugin-checker";
 
 export default defineConfig({
-    plugins: [handlebars()],
+    plugins: [handlebars(), checker({
+        typescript: true,
+    })],
     build: {
         outDir: 'dist'
     }
